b6b2f287795efae0e093ccb7dbecd2dd973f0150
con0fav <connor.favero@gmail.com>
2021-02-22 17:48:58 -0800

    started writing api routes, slightly modified variable in html routes

---
+++ routes/apiRoutes.js
@@ -0,0 +1,17 @@
+const db = require("../models");
+
+module.exports = function(app) {
+
+    app.get("/api/workouts", (req, res) => {
+        db.Workout.find({})
+        .then(workout => {
+            res.json(workout);
+        })
+        .catch(err => {
+            res.json(err);
+        });
+    });
+
+
+
+}
