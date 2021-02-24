d2bbf8f84d172d0f0407baf60e57441fd5acf1a6
con0fav <connor.favero@gmail.com>
2021-02-23 19:47:27 -0800

    range lookup and post routes

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -12,6 +12,24 @@ module.exports = function(app) {
         });
     });
 
+    app.get("/api/workouts/range", ({}, res) => {
+        db.Workout.find({})
+        .then(workout => {
+            res.json(workout);
+        })
+        .catch(err => {
+            res.json(err);
+        });
+    });
 
+    app.post ("api/workouts", (req, res) => {
+        db.Workout.create(req.body)
+        .then((workout) =>  {
+            res.json(workout);
+        })
+        .catch(err => {
+            res.json(err);
+        });
+    });
 
 }
